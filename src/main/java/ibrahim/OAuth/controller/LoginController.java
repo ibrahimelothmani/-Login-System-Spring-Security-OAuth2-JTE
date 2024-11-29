4c6dd7ce1a5b1d294dc19fc06fcb8bc3aef84ed3
Ibrahim El Othmani <ibrahimelothmani98@gmail.com>
2024-11-29 18:40:48 +0100

    Login-System-Spring-Security-OAuth2-JTE

---
+++ src/main/java/ibrahim/OAuth/controller/LoginController.java
@@ -0,0 +1,27 @@
+package ibrahim.OAuth.controller;
+
+
+import org.springframework.ui.Model;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String login(HttpServletRequest request, Model model, String error, String logout) {
+
+        if (error != null) {
+            model.addAttribute("error", true);
+            model.addAttribute("errorMessage", "Invalid username or password");
+        }
+
+        return "pages/login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "pages/home";
+    }
+}
