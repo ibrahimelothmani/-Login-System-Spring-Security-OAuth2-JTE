b8b001c5b6b3f1f9456a96f442655f85eeba688d
Ibrahim El Othmani <ibrahimelothmani98@gmail.com>
2024-12-02 22:09:58 +0100

    Login-System-Spring-Security-OAuth2-JTE

--- src/main/java/ibrahim/OAuth/OAuthApplication.java
+++ src/main/java/ibrahim/OAuth/OAuthApplication.java
@@ -1,13 +1,17 @@
 package ibrahim.OAuth;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OAuthApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(OAuthApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(OAuthApplication.class, args);
+		logger.info("OAuth Application started");
 	}
 
 }
